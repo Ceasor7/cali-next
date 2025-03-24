fe2ac43693b7aaf2d3394934bcf4166f24ab0cb0
Joseph Kimani07 <josegee0707@gmail.com>
2025-03-24 16:27:06 +0300

    minor changes 2

--- components/Values/ValuesCard.tsx
+++ components/Values/ValuesCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 const ValuesCard = ({ coreTitle }: Props) => {
   return (
     <div className=" rounded-xl shadow-2xl w-72 md:w-96 mx-auto p-10">
-      <p className=" text-center">{coreTitle}</p>
+      <p className=" text-center font-bold">{coreTitle}</p>
     </div>
   );
 };
